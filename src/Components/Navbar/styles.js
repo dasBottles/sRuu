5db2ed8f4444b6c79b83c8b8361cd259e5f820a2
David Le <arattanasack@gmail.com>
2020-12-06 03:56:23 -0800

    adjust minor styling

--- src/Components/Navbar/styles.js
+++ src/Components/Navbar/styles.js
@@ -1,30 +1,29 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from "styled-components";
 
 export const Styles = styled.div`
-.logoImg {
+  .logoImg {
     max-height: 150px;
     max-width: 150px;
-}
+  }
 
-a {
+  a {
     text-decoration: none !important;
     padding: -5px 0px -5px 0px !important;
-}
+  }
 
-.nav  {
+  a:hover {
+    color: white;
+  }
+
+  .nav {
     margin-bottom: 5%;
     /* background: #8bc0de; // color 1/ */
-    /* background: #c4ebfa; // color 2/cyan
-    background: #fcf2af; // color 3 */
-    background: #fff7d6; //color 4
-
-    /* opacity: 75%; */
-} 
-.navLink {
+    background: #c4ebfa; // color 2/cyan
+    /* background: #fcf2af; // color 3
+    background: #fff7d6; //color 4 */
+  }
+  .navLink {
     color: #fcf2af; //color 3
     /* color: #fff7d6; //color 4 */
-
-}
-
-
-`
+  }
+`;
